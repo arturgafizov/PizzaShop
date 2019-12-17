784aa25d12799e4867c410616e12996eac857787
Artur Gafizov <art05xxx@rambler.ru>
2019-12-17 20:02:26 +0300

    Change small havaiin image, Display products, step 3, add image

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_15_130710) do
+ActiveRecord::Schema.define(version: 2019_12_17_051046) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
